b63984f1e6e67879b551c9ee9b9032adc7d7882a
jeonghye-choi <ppllhm.0@gmail.com>
2022-09-02 20:51:51 +0900

    feat: eRC-721 contract setup

---
+++ packages/solidity/browser/ERC-721.sol
@@ -0,0 +1,54 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+/* is ERC165 */ 
+interface ERC721 {
+  event Transfer(
+    address indexed _from, 
+    address indexed _to, 
+    uint256 indexed _tokenId
+  );
+  event Approval(
+    address indexed _owner, 
+    address indexed _approved, 
+    uint256 indexed _tokenId
+  );
+  event ApprovalForAll(
+    address indexed _owner, 
+    address indexed _operator, 
+    bool _approved
+  );
+
+  function balanceOf(address _owner) public view returns (uint256);
+
+  function ownerOf(uint256 _tokenId) public view returns (address);
+
+  function safeTransferFrom(
+    address _from, 
+    address _to, 
+    uint256 _tokenId, 
+    bytes data
+  ) public;
+
+  function safeTransferFrom(
+    address _from, 
+    address _to, 
+    uint256 _tokenId
+  ) public;
+
+  function transferFrom(
+    address _from, 
+    address _to, 
+    uint256 _tokenId
+  ) public;
+
+  function approve(address _approved, uint256 _tokenId) public;
+
+  function setApprovalForAll(address _operator, bool _approved) public;
+
+  function getApproved(uint256 _tokenId) public view returns (address);
+
+  function isApprovedForAll(
+    address _owner, 
+    address _operator
+  ) public view returns (bool);
+}
